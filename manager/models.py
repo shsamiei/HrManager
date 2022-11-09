fb89000e069f76a48af6030762d5db24187b7a53
hossein <shsamiei27ap@gmail.com>
2022-11-09 20:58:41 +0330

    four classes added

--- manager/models.py
+++ manager/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 
+class Salary(models.Model):
+    value = models.IntegerField()
+    paid_date = models.DateField(auto_now_add=True)
+
+
 class BaseEmployee(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
     national_number = models.IntegerField()
+    salary = models.ForeignKey(Salary, on_delete=models.CASCADE)
     
+    
+
+class Employee(BaseEmployee):
+    pass
+
+class HrManager(BaseEmployee):
+    pass
+
+class PayRollManager(BaseEmployee):
+    pass
+
 
